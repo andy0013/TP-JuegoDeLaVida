efd3e3ef039d59f10063d93436241af9ead51ee4
andy0013 <38819430+andy0013@users.noreply.github.com>
2018-10-06 19:12:29 -0300

    Update testeo.cpp

--- testeo.cpp
+++ testeo.cpp
@@ -7,19 +7,19 @@
 
 #include "Parcela.h"
 #include "Celula.h"
+#include "Interaccion.h"
 #include <iostream>
+
+#include "Tablero.h"
 using namespace std;
 
 int main(){
-	int vidaCelula,vidaCelulaPorUsuario;
-	Parcela parcela(0.9);//Nace por otras 3 celulas , entonces tiene su propio coeficiente de nacimiento , lo paso por parametro ahora hasta armar tablero.
-	Parcela parcelaNacidaPorUsuario(0.5,true);//La que nace por el usuario nace con 100%
-	vidaCelula = parcela.celulaDarVida();
-	vidaCelulaPorUsuario = parcelaNacidaPorUsuario.celulaDarVida();
-	cout << vidaCelula << " % " << endl;
-	cout << vidaCelulaPorUsuario << " % " << endl;
-	vidaCelulaPorUsuario = parcelaNacidaPorUsuario.celulaDarVida();
-	cout << vidaCelulaPorUsuario << " % " << endl;//Al volver a nacer , si sucediera , seria con su respectivo coeficiente.
-	return 0;
+	Interaccion locutor;
+	int f,c;
+	f = locutor.solicitarNumeroDeFilas();
+	c = locutor.solicitarNumeroDeColumnas();
+	tablero juego(f,c);
+	juego.iniciarCelulas();
+	juego.mostrarTableroDeJuego();
 }
 
