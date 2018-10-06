853c98b9e20a1fa3f821c26b0348ecf7c7db175d
andy0013 <38819430+andy0013@users.noreply.github.com>
2018-10-06 19:11:32 -0300

    Update Parcela.cpp

--- Parcela.cpp
+++ Parcela.cpp
@@ -6,41 +6,38 @@
  */
 
 #include "Parcela.h"
+#include <iostream>
 
 namespace std {
 
-Parcela::Parcela(float factorNacimiento,bool primeraVez){
+
+Parcela::Parcela() {
 	red = 255;
 	green = 255;
 	blue = 255;
-	this->factorDeNacimiento = factorNacimiento;
-	this->porUsuario = primeraVez;
+	this->factorDeNacimiento = 0.9;
 }
 
-Parcela::Parcela(float factorNacimiento) {
-	red = 255;
-	green = 255;
-	blue = 255;
-	this->porUsuario = false;
-	this->factorDeNacimiento = factorNacimiento;
-}
-
-float Parcela::celulaDarVida(){
-	float estadoCelula;
-	if(this->porUsuario){
-		celula.nacer(1);
-		this->porUsuario = false;//solo una vez va a iniciar con 100% que sera cuando le de vida el usuario , luego siempre usara el coeficiente si nace.
-	}else{
-		celula.nacer(this->factorDeNacimiento);
-	}
-	estadoCelula = celula.obtenerEstado();
-	return estadoCelula;
+void Parcela::celulaDarVida(){
+	celula.nacer(this->factorDeNacimiento);
 }
 
 float Parcela::obtenerPromedioColor(){
 	float promedio;
 	promedio = (this->blue + this->green + this->red)/3;
 	return promedio;
+}
+	
+/* 
+OBTENER DATO ESTADO CELULA AUN POR DEFINIRSE DEPENDE DE CUANDO PASEMOS EL OBJETO A LA MATRIZ
+*/
+void Parcela::obtenerDatoEstadoDeCelula(){
+	char estado;
+	if(celula.obtenerEstado()){
+		cout << "37" << endl;
+	}else{
+		cout << "47" << endl;;
+	}
 }
 
 } /* namespace std */
