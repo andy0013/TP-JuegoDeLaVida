2bf7d31b8572751e2077db3db639a1e50c4d9ce3
andy0013 <38819430+andy0013@users.noreply.github.com>
2018-10-06 19:13:50 -0300

    Add files via upload

---
+++ Interaccion.h
@@ -0,0 +1,34 @@
+/*
+ * Interaccion.h
+ *
+ *  Created on: 06/10/2018
+ *      Author: andres
+ */
+
+#ifndef INTERACCION_H_
+#define INTERACCION_H_
+
+namespace std {
+
+class Interaccion {
+public:
+	Interaccion();
+
+	char ingresarCelula();
+
+	int solicitarNumeroDeColumnas();
+
+	int solicitarNumeroDeFilas();
+
+	int solicitarPosicionDeCelulaFila();
+
+	int solicitarPosicionDeCelulaColumna();
+
+	int solicitarNumeroDeTableros();
+
+	virtual ~Interaccion();
+};
+
+} /* namespace std */
+
+#endif /* INTERACCION_H_ */
