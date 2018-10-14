6eb5dc29a23cea59036d3f467d416571c3d32769
andy0013 <38819430+andy0013@users.noreply.github.com>
2018-10-14 20:27:33 -0300

    Update Tablero.cpp

--- src/Tablero.cpp
+++ src/Tablero.cpp
@@ -13,36 +13,51 @@
 namespace std {
 
 tablero::tablero(int numFilas,int numColumnas) {
+	
 	this->celulasVivas = 0;
+	
 	this->numeroDeFilas = numFilas;
+	
 	this->numeroDeColumnas = numColumnas;
+	
 	this->matriz = new char*[this->numeroDeFilas];
+	
 	for ( int i = 0 ; i < this->numeroDeFilas ; i ++){
+		
 		this->matriz[i] = new char[this->numeroDeColumnas];
 	}
 	iniciarTablero();
 }
 
 void tablero::crearCelulas(int fila,int columna){
+	
 	*(*(this->matriz + fila) + columna) = 79;
 }
 
 void tablero::iniciarCelulas(){
 	int fila,columna;
+	
 	char respuesta;
+	
 	do{
 		fila = consola.solicitarPosicionDeCelulaFila();
+		
 		columna = consola.solicitarPosicionDeCelulaColumna();
 
 		crearCelulas(fila,columna);
+		
 		this->celulasVivas++;
+		
 		respuesta = consola.ingresarCelula();
+		
 	}while(respuesta == 'S');
 }
 
 void tablero::iniciarTablero(){
 	for(int i = 0 ; i < this->numeroDeFilas ; i++ ){
+		
 		for(int j = 0 ; j < this->numeroDeColumnas ; j++ ){
+			
 			*(*(this->matriz + i) + j) = 35;
 		}
 	}
@@ -51,7 +66,9 @@ void tablero::iniciarTablero(){
 
 void tablero::mostrarTableroDeJuego(){
 	for(int i = 0 ; i < this->numeroDeFilas ; i++ ){
+		
 		for(int j = 0 ; j < this->numeroDeColumnas ; j++ ){
+			
 			cout << *(*(this->matriz + i) + j);
 		}
 	cout << endl;
@@ -60,6 +77,7 @@ void tablero::mostrarTableroDeJuego(){
 
 tablero::~tablero() {
 	for(int i = 0 ; i < this->numeroDeFilas ; i++ ){
+		
 			delete [] this->matriz[i];
 	}
 
